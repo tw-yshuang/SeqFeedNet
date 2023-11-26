be4cff13c757ae7b3b445ce49e981ce77f1fe12e
tw-yshuang <tw.yshuang@gmail.com>
2023-11-27 02:55:00 +0800

    [rename][add][fix][mod] rename:     evaluation/eval_utils.py -> utils/evalutate/eval_utils.py add:     utils/DataID_MatchTable.py, move match table from utils/evalutate/eval_utils.py to here fix:     utils/data_process.py, dataset is passed, but in the dataloader got TypeError: default_collate: batch must contain tensors, numpy arrays, numbers, dicts or lists; so we change it to deliver id information

--- utils/data_process.py
+++ utils/data_process.py
@@ -14,8 +14,9 @@ if __name__ == '__main__':
 
     sys.path.append(str(Path(__file__).resolve().parents[1]))
 
-from submodules.UsefulFileTools.FileOperator import get_filenames, str_format
+from submodules.UsefulFileTools.FileOperator import get_filenames
 from cross_validation_config import datasets_tr, datasets_test
+from utils.DataID_MatchTable import ID2VID, ID2CAT, VID2ID, CAT2ID
 from utils.data_preprocess import CDNet2014Preprocess
 from utils.transforms import CustomCompose
 
@@ -38,6 +39,7 @@ class DatasetConfig:
 class CDNet2014OneVideo:
     def __init__(self, cate_name: str, name: str) -> None:
         self.name = name
+        self.id = VID2ID[self.name]
 
         self.temporalROI: Tuple[int]
         self.gtPaths_all: Tuple[str]
@@ -85,6 +87,7 @@ class CDNet2014OneVideo:
 class CDNet2014OneCategory:
     def __init__(self, name: str, ls: List[str]) -> None:
         self.name = name
+        self.id = CAT2ID[self.name]
         self.videos = [CDNet2014OneVideo(self.name, video_str) for video_str in ls]
 
     def __repr__(self) -> str:
@@ -145,19 +148,27 @@ class CDNet2014Dataset(Dataset):
         features = torch.from_numpy(self.__get_features(video).transpose(0, 3, 1, 2)).type(torch.float32) / 255
 
         if not self.isTrain:
-            return (cate, video), features, self.__getitem4testIter(video)
+            # ! test_dataset can use this, but test_loader can not use.
+            # ! TypeError: default_collate: batch must contain tensors, numpy arrays, numbers, dicts or lists; found <class 'generator'>
+            # * video_info, features, genFunc4FrameAndLabel(); genFunc4FrameAndLabel() -> frame, label
+            return (cate.id, video.id), features, self.__getitem4testIter(video)
 
         frame_ids = self.__get_frameIDs(video, frame_id)
         frame_ls = []
         label_ls = []
-        for i in frame_ids:
-            frame_ls.append(cv2.imread(video.inputPaths_inROI[i], cv2.COLOR_BGR2RGB))
-            label_ls.append(np.expand_dims(self.preprocess(cv2.imread(video.gtPaths_inROI[i], cv2.IMREAD_GRAYSCALE)), axis=-1))
+        try:
+            for i in frame_ids:
+                frame_ls.append(cv2.imread(video.inputPaths_inROI[i], cv2.COLOR_BGR2RGB))
+                label_ls.append(np.expand_dims(self.preprocess(cv2.imread(video.gtPaths_inROI[i], cv2.IMREAD_GRAYSCALE)), axis=-1))
+        except IndexError:
+            print(frame_ids, len(video.inputPaths_inROI))
+            exit()
 
         frames = torch.from_numpy(np.stack(frame_ls).transpose(0, 3, 1, 2)).type(torch.float32) / 255.0
         labels = torch.from_numpy(np.stack(label_ls).transpose(0, 3, 1, 2))
 
-        return (cate, video), *self.transforms_cpu(features, frames, labels)  # video_info, features, frames, labels
+        # * video_info, features, frames, labels
+        return (cate.id, video.id), *self.transforms_cpu(features, frames, labels)
 
     def __getitem4testIter(self, video: CDNet2014OneVideo):
         for input_path, gt_path in zip(video.inputPaths_inROI, video.gtPaths_inROI):
@@ -169,20 +180,21 @@ class CDNet2014Dataset(Dataset):
     # *dataset selecting strategy
     def __get_frameIDs(self, video: CDNet2014OneVideo, start_id: int) -> List[int]:
         len_frame = len(video.inputPaths_inROI)
+
         if len_frame - start_id < self.cfg.frame_groups * self.gap:
-            return sorted(random.sample(range(start_id, len_frame), k=self.cfg.frame_groups))
+            return sorted(random.sample(range(start_id - self.cfg.frame_groups * self.gap - 1, len_frame), k=self.cfg.frame_groups))
 
         frame_ids: List[int] = []
         frame_id = start_id
         for _ in range(self.cfg.frame_groups):
-            frame_id += random.randint(1, self.gap + 1)
+            frame_id += random.randint(1, self.gap)
             frame_ids.append(frame_id)
 
         return frame_ids
 
     def __get_features(self, video: CDNet2014OneVideo, mean=0, std=128):
         f0 = cv2.imread(random.choice(video.emptyBgPaths), cv2.COLOR_BGR2RGB)
-        f1 = f0 + np.random.normal(0, 180, f0.shape)
+        f1 = f0 + np.random.normal(mean, std, f0.shape)
 
         return np.stack([f0, f1])
 
@@ -200,21 +212,23 @@ def get_dataloader(
     batch_size: int = 32,
     num_workers: int = 8,
     pin_memory: bool = False,
-    compose: CustomCompose | transforms.Compose = None,
+    train_transforms_cpu: CustomCompose = None,
+    test_transforms_cpu: transforms.Compose = None,
     label_isShadowFG: bool = False,
     **kwargs,
 ):
-    assert isinstance(compose, (CustomCompose)), "compose must be CustomCompose or transforms.Compose"
-
-    dataset = CDNet2014Dataset(datasets_tr, cv_set, dataset_cfg, compose, isTrain=True)
+    dataset = CDNet2014Dataset(datasets_tr, cv_set, dataset_cfg, train_transforms_cpu, isShadowFG=label_isShadowFG, isTrain=True)
     train_len = int(len(dataset) * dataset_rate)
     train_set, val_set = random_split(dataset, [train_len, len(dataset) - train_len])
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
-    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
+    if dataset_rate != 1.0:
+        val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
+    else:
+        val_loader = None
 
-    test_dataset = CDNet2014Dataset(datasets_test, cv_set, dataset_cfg)
+    test_set = CDNet2014Dataset(datasets_test, cv_set, None, test_transforms_cpu, isShadowFG=label_isShadowFG, isTrain=False)
 
-    return train_set, val_set, train_loader, val_loader
+    return train_loader, val_loader, test_set
 
 
 if __name__ == '__main__':
@@ -253,7 +267,7 @@ if __name__ == '__main__':
 
     # =============================
 
-    trans = transforms.Compose(
+    test_trans = transforms.Compose(
         [
             transforms.Resize(size=(244, 244)),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -268,10 +282,57 @@ if __name__ == '__main__':
     #     ]
     # )
 
-    data_test = CDNet2014Dataset(cv_dict=datasets_test, cv_set=5, transforms_cpu=trans, isTrain=False)
+    data_test = CDNet2014Dataset(cv_dict=datasets_test, cv_set=5, transforms_cpu=test_trans, isTrain=False)
     info, features, iterFandL = next(iter(data_test))
 
     print(info)
     print(features)
     for i, (frame, label) in enumerate(iterFandL):
         print(i)
+
+    # =============================
+
+    cfg = DatasetConfig()
+    trans = CustomCompose(
+        [
+            # transforms.ToTensor(), # already converted in the __getitem__()
+            transforms.RandomCrop(size=(224, 224)),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+    test_trans = transforms.Compose(
+        [
+            transforms.Resize(size=(244, 244)),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+
+    train_loader, val_loader, test_set = get_dataloader(
+        dataset_cfg=cfg,
+        cv_set=5,
+        dataset_rate=1,
+        batch_size=8,
+        num_workers=16,
+        pin_memory=True,
+        train_transforms_cpu=trans,
+        test_transforms_cpu=test_trans,
+        label_isShadowFG=False,
+    )
+
+    for i, (video_info, features, frames, labels) in enumerate(train_loader):
+        print(f"train: {i}")
+        if i == 0:
+            print(video_info)
+            print(features)
+            print(frames)
+            print(labels)
+
+    for i, (video_info, features, iterFandL) in enumerate(test_set):
+        print(f"test: {i}")
+        if i == 0:
+            print(video_info)
+            print(features)
+        for j, (frame, label) in enumerate(iterFandL):
+            print(f"iter: {j}")
+            if j == 0:
+                print()
