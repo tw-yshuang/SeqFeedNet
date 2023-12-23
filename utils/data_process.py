[fix] method: CDNet2014Dataset.getitem(), {frame_ls, label_ls} get wrong index, fixed it

utils/data_process.py
@@ -194,10 +194,10 @@ class CDNet2014Dataset(Dataset):
         frame_ls = []
         empty_ls = []
         label_ls = []
-        for i in frame_ids:
-            frame_ls.append(read_image(video.inputPaths_inROI[i])[[2, 1, 0]])
+        for i, idx in enumerate(frame_ids):
+            frame_ls.append(read_image(video.inputPaths_inROI[idx])[[2, 1, 0]])
             empty_ls.append(read_image(emptyBg4InputPaths[i])[[2, 1, 0]])
-            label_ls.append(self.preprocess(read_image(video.gtPaths_inROI[i])))
+            label_ls.append(self.preprocess(read_image(video.gtPaths_inROI[idx])))
 
         frames = torch.stack(frame_ls).type(torch.float32) / 255.0
         empty_frames = torch.stack(empty_ls).type(torch.float32) / 255.0