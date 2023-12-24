55b9f47ca11cd6b62a94f8cd1b9a6c78e1cf7189
tw-yshuang <tw.yshuang@gmail.com>
2023-12-24 08:20:32 +0800

    [add][mod] add UNet3D for SEM add:     {models/Unet3D.py, models/unet3d_tools/*}, UNet3D Model mod:     models/SEMwithMEM.py, method: SMNet2D.forward(), bg_only_imgs.unsqueeze() is in the mehod now; add-class: SMNet3to2D, for 3D SEM to 2D MEM used     Processor.py, add the code to fit the 3d SEM use

--- Processor.py
+++ Processor.py
@@ -14,6 +14,7 @@ from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
 
 from models.unet import *
+from models.Unet3D import *
 from models.SEMwithMEM import *
 from utils.evaluate.losses import *
 from models.unet import UNetVgg16 as BackBone
@@ -264,7 +265,7 @@ class Processor:
             with torch.no_grad():
                 frames, labels, features = transforms(frames, labels, features)
 
-            bg_only_imgs = features[:, 0].unsqueeze(1)
+            bg_only_imgs = features[:, 0]
             for step in range(frames.shape[1]):
                 frame, label = frames[:, step], labels[:, step]
 
@@ -474,7 +475,7 @@ def execute(parser: Parser):
     )
 
     #! ========== Network ==========
-    se_model: nn.Module = parser.SE_Net(9, 6)
+    se_model: nn.Module = parser.SE_Net(3, 2) if '3D' in parser.SE_Net.__class__.__name__ else parser.SE_Net(9, 6)
     me_model: nn.Module = parser.ME_Net(9, 1)
     sm_net: nn.Module = parser.SM_Net(se_model, me_model).to(parser.DEVICE)
     optimizer: optim.Optimizer = parser.OPTIMIZER(sm_net.parameters(), lr=parser.LEARNING_RATE, weight_decay=parser.WEIGHT_DECAY)
@@ -498,7 +499,7 @@ def execute(parser: Parser):
         saveDir += path.split('_')[0]
     else:
         model_name = f'{sm_net.__class__.__name__}.{se_model.__class__.__name__}-{me_model.__class__.__name__}'
-        optimizer_name = f'{optimizer.__class__.__name__}{optimizer.defaults["lr"]:.1e}.{parser.WEIGHT_DECAY:.1e}'
+        optimizer_name = f'{optimizer.__class__.__name__}{optimizer.defaults["lr"]:.1e}.wd{parser.WEIGHT_DECAY}'
         saveDir += f'{time.strftime("%m%d-%H%M")}_{parser.OUT}_{model_name}_{optimizer_name}_{str(loss_func)}_BS-{parser.BATCH_SIZE}_Set-{parser.CV_SET}'
 
     check2create_dir(saveDir)
--- models/SEMwithMEM.py
+++ models/SEMwithMEM.py
@@ -7,8 +7,6 @@ if __name__ == '__main__':
 
     sys.path.append(str(Path(__file__).resolve().parents[1]))
 
-from models.unet import UNetVgg16
-
 
 class StandardNorm(nn.Module):
     def __init__(self, noise_rate=1e-4, dim=0, *args, **kwargs) -> None:
@@ -32,8 +30,11 @@ class SMNet2D(nn.Module):
     def forward(self, frame: torch.Tensor, features: torch.Tensor, bg_only_imgs: torch.Tensor):
         frame = frame.squeeze(1)
 
-        combine_features = torch.hstack((features, bg_only_imgs))
-        if combine_features.dim() == 5:
+        combine_features: torch.Tensor
+        if features.dim() != 5:
+            combine_features = torch.hstack((features, bg_only_imgs))
+        else:
+            combine_features = torch.hstack((features, bg_only_imgs.unsqueeze(1)))
             combine_features = combine_features.reshape(
                 combine_features.shape[0],
                 combine_features.shape[1] * combine_features.shape[2],
@@ -43,5 +44,24 @@ class SMNet2D(nn.Module):
         features = self.se_model(combine_features)
 
         mask = self.me_model(torch.hstack((features, frame)))
+
+        return mask, frame, features
+
+
+class SMNet3to2D(nn.Module):
+    def __init__(self, se_model: nn.Module, me_model: nn.Module, *args, **kwargs) -> None:
+        super(SMNet3to2D, self).__init__(*args, **kwargs)
+
+        self.se_model = se_model
+        self.me_model = me_model
+
+    def forward(self, frame: torch.Tensor, features: torch.Tensor, bg_only_imgs: torch.Tensor):
+        frame = frame.squeeze(1)
+        bg_only_imgs = bg_only_imgs.unsqueeze(1)
+
+        features = self.se_model(torch.hstack((features, bg_only_imgs)))
+
+        b, c, d = features.shape[:3]
+        mask = self.me_model(torch.hstack((features.reshape(b, c * d, *features.shape[3:]), frame)))
 
         return mask, frame, features
