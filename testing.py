5e7dbacde6d2a845353286f1e950aba3ca31e8b9
tw-yshuang <tw.yshuang@gmail.com>
2024-02-03 11:40:07 +0800

    [mod] testing.py, with previous commit

--- testing.py
+++ testing.py
@@ -3,7 +3,8 @@ import click
 help_doc = {
     'pretrain_weight': "Pretrain weight, model structure must same with the setting",
     'cv_set_number': "Cross validation set number for training and test videos will be selected",
-    'save': "Save all the predict results",
+    'only_temporalROI': "Only do testing evaluation during temporalROI",
+    'save_result': "Save all the predict results",
     'device': "CUDA ID, if system can not find Nvidia GPU, it will use CPU",
     'output': "Model output directory",
 }
@@ -12,12 +13,14 @@ help_doc = {
 @click.command(context_settings=dict(help_option_names=['-h', '--help'], max_content_width=120))
 @click.option('-weight', '--pretrain_weight', default='', help=help_doc['pretrain_weight'])
 @click.option('-cv', '--cv_set_number', default=1, help=help_doc['cv_set_number'])
-@click.option('-save', '--save_result', default=False, is_flag=True, help=help_doc['save'])
+@click.option('-onlyROI', '--only_temporalROI', default=False, is_flag=True, help=help_doc['only_temporalROI'])
+@click.option('-save', '--save_result', default=False, is_flag=True, help=help_doc['save_result'])
 @click.option('--device', default=0, help=help_doc['device'])
 @click.option('-out', '--output', default='', help=help_doc['output'])
 def cli(
     cv_set_number: int,
     device: int,
+    only_temporalROI: bool,
     save_result: bool,
     pretrain_weight: str,
     output: str,
@@ -30,6 +33,7 @@ def cli(
         cv_set_number=cv_set_number,
         device=device,
         pretrain_weight=pretrain_weight,
+        test_from_begin=~only_temporalROI,
         save_test_result=save_result,
         output=output,
     )
