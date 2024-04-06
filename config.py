9fcfa5a4dcdecab02e575d008924f81a65fe21f4
tw-yshuang <tw.yshuang@gmail.com>
2024-04-06 19:20:12 +0800

    [mod] remove development code

--- config.py
+++ config.py
@@ -5,7 +5,3 @@ class DatasetConfig:
     frame_groups = 5
     gap_range = [2, 20]
     sample4oneVideo = 200
-    num_epochs = 100
-
-    def __init__(self, isModel3D=True) -> None:
-        self.concat_axis = -1 if isModel3D else 0  # axis dependent by model input_channel
